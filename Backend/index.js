f36319075a880927bcf04d1b7d670508cd1d1adf
Sandeep Kumar <sk245444@gmail.com>
2025-09-23 14:34:47 +0530

    update some issue fix

--- Backend/index.js
+++ Backend/index.js
@@ -19,6 +19,8 @@ import errorHandler from './middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
+app.set('trust proxy', 1);
+
 const PORT = process.env.PORT || 5000;
 connectDB();
 
